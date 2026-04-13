08bbdf800ab8badfbfe90e18c170fc28e37d5902
Zancmok <nejc.germovsek@gmail.com>
2026-04-13 22:35:24 +0200

    Played a bit with react and tailwind

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home()
+{
+    return <div>
+        <h1 className="text-5xl font-bold mb-4">
+            Til~san 🎧
+        </h1>
+        <p className="text-gray-400">
+            Welcome to KCSpotifyAnalyzer
+        </p>
+    </div>
+}
