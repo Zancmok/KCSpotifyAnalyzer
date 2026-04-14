d275549f12dde11c26ba8f5025a6b1e3e7b812c2
Zancmok <145902998+Zancmok@users.noreply.github.com>
2026-04-14 08:58:04 +0000

    Did stuff

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,24 @@
 export default function Home()
 {
-    return <div>
-        <h1 className="text-5xl font-bold mb-4">
-            Til~san 🎧
-        </h1>
-        <p className="text-gray-400">
-            Welcome to KCSpotifyAnalyzer
-        </p>
-    </div>
+    function onClick(): void
+    {
+        console.log("Button clicked!");
+    }
+
+    return (
+        <div className="min-h-screen text-white flex flex-col items-center justify-center px-4">
+            <h1 className="text-4xl md:text-5xl font-bold py-4 text-green-600">
+                KC SpotifyAnalyzer
+            </h1>
+
+            <p className="standart-text text-center max-w-xl mb-8">
+                KC SpotifyAnalyzer is a tool for advanced analysis of your Spotify data. 
+                Discover listening patterns, explore detailed statistics, and compare your music taste with your friends.
+            </p>
+
+            <button onClick={onClick} className="green-button">
+                Get Started
+            </button>
+        </div>
+    );
 }
