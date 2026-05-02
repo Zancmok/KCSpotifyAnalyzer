5315142e1050f51a42aa180bcf140127381482b6
Zancmok <nejc.germovsek@gmail.com>
2026-05-02 11:58:25 +0200

    Auth works fully!

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/users', function () {
-    return User::all();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [UserController::class, 'me']);
 });
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\SpotifyController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/auth/spotify/redirect', [SpotifyController::class, 'redirect']);
 Route::get('/auth/spotify/callback', [SpotifyController::class, 'callback']);
