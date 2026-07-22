e110f7390ee4cb137b2144f4a5b43c4e1d9224b2
Zancmok <145902998+Zancmok@users.noreply.github.com>
2026-07-22 12:18:05 +0200

    Enhance exception chaining in _load_float
    Improve error handling in _load_float function.

--- src/data_analyzer/config.py
+++ src/data_analyzer/config.py
@@ -23,8 +23,8 @@ def _load_bool(env_variable_name: str) -> bool:
 def _load_float(env_variable_name: str) -> float:
     try:
         return float(_load_env(env_variable_name))
-    except ValueError:
-        raise EnvironmentError(f".env is not of type float: '{env_variable_name}'")
+    except ValueError as exc:
+        raise EnvironmentError(f".env is not of type float: '{env_variable_name}'") from exc
 
 
 # General
